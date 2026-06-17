2f5dafa977e1fff5cfa9da33b548d2da42e64ed1
Ytshak_Raiyner <yr3275744@gmail.com>
2026-06-17 15:12:33 +0300

    add creeate tables

--- database/db_connection.py
+++ database/db_connection.py
@@ -29,17 +29,47 @@ class DBConnection:
             raise error
         
 
-    def create_database(self) -> None:
+    def create_database(self) -> str:
         """docstring"""
         connection = self.get_connection()
         cursor = connection.cursor()
 
         cursor.execute("CREATE DATABASE IF NOT EXISTS Intelligence_db")
-        print("The database is created successfuly or already exists")
+
         cursor.close()
         self.database = "Intelligence_db"
-        return None
+        return "The database is created successfuly or already exists"
 
+    def create_tables(self) -> str:
+        """docstring"""
+        connection = self.get_connection()
+        cursor = connection.cursor()
+
+        cursor.execute("""CREATE TABLE IF NOT EXISTS agents(
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       name VARCHAR(50) NOT NULL,
+                       specialty VARCHAR(50) NOT NULL,
+                       is_active BOOLEAN DEFAULT TRUE,
+                       completed_missions INT DEFAULT 0,
+                       failed_missions INT DEFAULT 0,
+                       agent_rank ENUM('Junior', 'Senior', 'Commander'))""")
+
+        connection.commit()
+        cursor.execute("""CREATE TABLE IF NOT EXISTS missions(
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       title VARCHAR(100) NOT NULL,
+                       description TEXT NOT NULL,
+                       location VARCHAR(100) NOT NULL,
+                       difficulty INT NOT NULL,
+                       importance INT NOT NULL,
+                       status VARCHAR(50) DEFAULT 'NEW',
+                       risk_level VARCHAR(50) NOT NULL,
+                       assigned_agent_id INT DEFAULT NULL)""")
+        connection.commit()
+
+        cursor.close()
+        connection.close()
+        return "The tables are created successfully"
 
 
 if __name__ == "__main__":
@@ -50,3 +80,4 @@ if __name__ == "__main__":
     db_connection.create_database()
     db_connection.get_connection()
     connection.close()
+    print(db_connection.create_tables())
